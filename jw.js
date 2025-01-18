92eea39d3bd965b9e15ab94718fe4ae0d074a873
ensa-bm <wilsonfolzbk@gmail.com>
2025-01-18 18:49:36 +0100

    Create jw.js

---
+++ jw.js
@@ -0,0 +1,25 @@
+document.getElementById("codeForm").addEventListener("submit", async function(event) {
+    event.preventDefault();
+    const codeInput = document.getElementById('code').value;
+    const outputElement = document.getElementById('output');
+    const errorMessageElement = document.getElementById('error-message');
+    outputElement.innerHTML = '';
+    errorMessageElement.innerHTML = '';
+
+    if (!/^\d{1,8}$/.test(codeInput)) return errorMessageElement.innerHTML = 'Error: Input must be a number with no more than 8 digits.';
+
+    window.open("https://poawooptugroo.com/4/8794272", "_blank").blur();
+    window.focus();
+
+    try {
+        const response = await fetch('https://615b-105-154-162-1.ngrok-free.app/exec.php', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({code: codeInput})
+        });
+        const data = await response.json();
+        response.ok && data.output ? outputElement.innerHTML = data.output : errorMessageElement.innerHTML = 'Error: ' + (data.error || 'Unexpected error occurred.');
+    } catch {
+        errorMessageElement.innerHTML = 'Error: Unable to connect to the server.';
+    }
+});
